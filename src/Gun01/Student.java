e32e2e60138dbafd50a9812734d4625f454f37e9
LENOVO <fkye_vuslat@hotmail.com>
2023-01-26 19:29:43 +0300

    task 2 yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -3,5 +3,7 @@ package Gun01;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
+
+        System.out.println("task 2 yapıldı");
     }
 }
