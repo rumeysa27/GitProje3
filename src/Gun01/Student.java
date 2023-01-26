59da83bf57c81261999264ba81524c6c9561a39a
LENOVO <fkye_vuslat@hotmail.com>
2023-01-26 19:38:45 +0300

    task 4 yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,8 +4,8 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
 
-        System.out.println("task 2 yapıldı");
 
-        System.out.println("tekrar merhaba");
+
+        System.out.println("tekrar merhaba ");
     }
 }
